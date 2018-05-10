3644b863cbe0462fd0f33bf6f09adb10fb18be0c
Baz-Co <shehbaz.sherwani@gmail.com>
2018-05-09 21:50:54 -0500

    Clean extraneous code

--- src/components/Products/ProductThumb/ProductThumb.js
+++ src/components/Products/ProductThumb/ProductThumb.js
@@ -2,20 +2,6 @@ import React, { Component } from "react"
 
 class ProductThumb extends Component {
 
-  // render() {
-  //   return (
-  //     <div>
-  //       <ProductThumb />
-  //       <h3>
-  //         White Belt
-  //       </h3>
-  //       <p>
-  //         Ying to the Yang of the Belt World.
-  //       </p>
-  //     </div>      
-  //   )
-  // }
-
   render() {
     return (
       <div>
